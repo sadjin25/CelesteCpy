ab3265d9a2d84985d789b474469c77af12b43685
sadjin25 <qkrdnwls0101@naver.com>
2023-03-05 13:48:02 +0900

    add: tilemap, background, etc

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,69 +5,59 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Transform groundChk;
+    private Rigidbody2D rb;
+    [SerializeField] private Transform groundChkr;
     [SerializeField] private LayerMask groundLayer;
 
-    private float horizontal;
-    private float speed = 8f;
+    private float direction;
+    private float maxSpeed = 10f;
     private float jumpingPower = 16f;
     private bool isFacingRight = true;
-    private bool isJumped = false;
+    public bool isGrounded;
 
-    private readonly float coyoteTime = 0.2f;
-    private float coyoteTimeCnt;
-
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
-        CoyoteTime();
+        GroundChk();
+        Flip();
     }
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-        Flip();
+        rb.velocity = new Vector2(direction * maxSpeed, rb.velocity.y);
     }
 
 
-    private void CoyoteTime()
-    {
-        if (IsGrounded())
-        {
-            coyoteTimeCnt = coyoteTime;
-        }
-
-        else
-        {
-            coyoteTimeCnt -= Time.deltaTime;
-        }
-    }
 
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if (context.performed && coyoteTimeCnt > 0f)
+        if (context.performed && isGrounded)
         {
+            isGrounded = false;
+
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
 
-        if (context.canceled && rb.velocity.y > 0f)
+        //BUG : while goes up, we release the button again n again, it goes up by jumppower * 0.5f.
+        if (context.canceled && rb.velocity.y > 0)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower * 0.5f);
-
-            coyoteTimeCnt = 0f;
         }
     }
 
-    private bool IsGrounded()
+    private void GroundChk()
     {
-        return Physics2D.OverlapCircle(groundChk.position, 0.2f, groundLayer);
+        isGrounded = Physics2D.OverlapCircle(groundChkr.position, 0.2f, groundLayer);
     }
 
     private void Flip()
     {
-        if (!isFacingRight && horizontal > 0f || isFacingRight && horizontal < 0f)
+        if (!isFacingRight && direction > 0f || isFacingRight && direction < 0f)
         {
             isFacingRight = !isFacingRight;
 
@@ -77,9 +67,9 @@ public class Player : MonoBehaviour
         }
     }
 
-    public void Move(InputAction.CallbackContext context)
+    public void GetInputDirection(InputAction.CallbackContext context)
     {
-        horizontal = context.ReadValue<Vector2>().x;
+        direction = context.ReadValue<Vector2>().x;
     }
 
 }
